8ed9f3f57f3a4e0247a9c82b6f002fc37457eff1
Tobias Piskula <tobias@piskula.at>
2022-05-07 20:17:50 +0200

    bp 18

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ const App = () => (
     <div>
       <iframe
         style={frameStyles}
-        src="https://jgw.challonge.com/bp18/module"
+        src="https://challonge.com/bp18/module"
         width="100%"
         height="500"
         frameborder="0"
